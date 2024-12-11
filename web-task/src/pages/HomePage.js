b4b0ca9410d4611d93870c413c5f7ca847baa542
Klevi-Alliu-Hybytes <ka21abv@herts.ac.uk>
2024-12-11 22:15:52 +0000

    created a feature section

--- web-task/src/pages/HomePage.js
+++ web-task/src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import Slider from "../components/Slider";
 import About from "../components/About";
 import Professional from "../components/Professional";
+import Feature from "../components/Feature";
 const HomePage = () => {
     return (
         <>
         <Slider />
+        <Feature />
         <About />
         <Professional />
         </>
