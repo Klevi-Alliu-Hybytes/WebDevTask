28da9679dcb0d322cf1675a0dd7ec7e59b4058f0
Klevi-Alliu-Hybytes <ka21abv@herts.ac.uk>
2024-12-11 21:52:33 +0000

    created about section

--- web-task/src/pages/HomePage.js
+++ web-task/src/pages/HomePage.js
@@ -1,8 +1,10 @@
 import Slider from "../components/Slider";
+import About from "../components/About";
 const HomePage = () => {
     return (
         <>
         <Slider />
+        <About />
         </>
     );
 };
