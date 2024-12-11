5f0e79877464476424188c1e5f52886e1724d316
Klevi-Alliu-Hybytes <ka21abv@herts.ac.uk>
2024-12-11 21:20:22 +0000

    fixed some minor issues with the header.js, index.js and the package.json

--- web-task/src/App.js
+++ web-task/src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
@@ -11,12 +10,12 @@ function App() {
   return (
       <Router>
         <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/services" element={<ServicesPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Routes>
+        {/*<Routes>*/}
+        {/*  <Route path="/" element={<HomePage />} />*/}
+        {/*  <Route path="/about" element={<AboutPage />} />*/}
+        {/*  <Route path="/services" element={<ServicesPage />} />*/}
+        {/*  <Route path="/contact" element={<ContactPage />} />*/}
+        {/*</Routes>*/}
       </Router>
   );
 }
--- web-task/src/components/Header.js
+++ web-task/src/components/Header.js
@@ -9,73 +9,77 @@ import "../assets/css/style.css";
 
 const Header = () => {
     return (
-        <header className="header_section">
-            {/* Top Header Section */}
-            <div className="header_top">
-                <div className="container-fluid">
-                    <div className="contact_nav">
-                        <a href="tel:+01123455678990">
-                            <FontAwesomeIcon icon={faPhone}/>
-                            <span>Call : +01 123455678990</span>
-                        </a>
-                        <a href="mailto:demo@gmail.com">
-                            <FontAwesomeIcon icon={faEnvelope}/>
-                            <span>Email : demo@gmail.com</span>
-                        </a>
+        <>
+
+            <header className="header_section">
+
+                {/* Top Header Section */}
+                <div className="header_top">
+                    <div className="container-fluid">
+                        <div className="contact_nav">
+                            <a href="tel:+01123455678990">
+                                <FontAwesomeIcon icon={faPhone}/>
+                                <span>Call : +01 123455678990</span>
+                            </a>
+                            <a href="mailto:demo@gmail.com">
+                                <FontAwesomeIcon icon={faEnvelope}/>
+                                <span>Email : demo@gmail.com</span>
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            {/* Bottom Header Section with Navigation */}
-            <div className="header_bottom">
-                <div className="container-fluid">
-                    <nav className="navbar navbar-expand-lg custom_nav-container">
-                        <Link className="navbar-brand" to="/">
-                            <span>Inance</span>
-                        </Link>
-                {/* Navbar Toggler for Mobile View */}
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className=""> </span>
-                </button>
-                        {/* Navigation Links */}
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav">
-                                <li className="nav-item active">
-                                    <Link className="nav-link" to="/">
-                                        Home
-                                    </Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/about">
-                                        About
-                                    </Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/services">
-                                        Services
-                                    </Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/contact">
-                                        Contact Us
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
+                {/* Bottom Header Section with Navigation */}
+                <div className="header_bottom">
+                    <div className="container-fluid">
+                        <nav className="navbar navbar-expand-lg custom_nav-container">
+                            <Link className="navbar-brand" to="/">
+                                <span>Inance</span>
+                            </Link>
+                            {/* Navbar Toggler for Mobile View */}
+                            <button
+                                className="navbar-toggler"
+                                type="button"
+                                data-toggle="collapse"
+                                data-target="#navbarSupportedContent"
+                                aria-controls="navbarSupportedContent"
+                                aria-expanded="false"
+                                aria-label="Toggle navigation"
+                            >
+                                <span className=""> </span>
+                            </button>
+                            {/* Navigation Links */}
+                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul className="navbar-nav">
+                                    <li className="nav-item active">
+                                        <Link className="nav-link" to="/">
+                                            Home
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/about">
+                                            About
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/services">
+                                            Services
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/contact">
+                                            Contact Us
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
                 </div>
-            </div>
-        </header>
+            </header>
+        </>
 
 
-                );
-                };
+    );
+};
 
 export default Header;
