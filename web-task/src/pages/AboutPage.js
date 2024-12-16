3d0ffda9bc3131d2c9bc131a355df31596f801f6
Klevi-Alliu-Hybytes <ka21abv@herts.ac.uk>
2024-12-16 19:31:26 +0000

    created teh about page

---
+++ web-task/src/pages/AboutPage.js
@@ -0,0 +1,16 @@
+import About from "../components/About";
+import Info from "../components/Info";
+import Footer from "../components/Footer";
+
+const AboutPage = () => {
+    return (
+        <>
+
+            <About />
+            <Info />
+            <Footer />
+        </>
+    )
+}
+
+export default AboutPage;
