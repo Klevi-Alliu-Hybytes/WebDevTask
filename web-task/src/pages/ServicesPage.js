667da06013efee9e97601074d682b0da1cbef801
Klevi-Alliu-Hybytes <ka21abv@herts.ac.uk>
2024-12-16 19:34:07 +0000

    created the service page

---
+++ web-task/src/pages/ServicesPage.js
@@ -0,0 +1,16 @@
+import Info from "../components/Info";
+import Footer from "../components/Footer";
+import Service from "../components/Service";
+
+const AboutPage = () => {
+    return (
+        <>
+
+            <Service />
+            <Info />
+            <Footer />
+        </>
+    )
+}
+
+export default AboutPage;
